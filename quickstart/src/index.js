06e64607ad858cc3d20a4c7b4773add34f27c4e3
Kuni <kuni.ujiie@grapecity.com>
2022-06-17 11:59:10 +0900

    Update index.js

--- quickstart/src/index.js
+++ quickstart/src/index.js
@@ -3,6 +3,6 @@ import repQuickStartSample from './pdfs/diodocs_pricelist.pdf';
 
 var viewer = new gcpdfviewer.GcPdfViewer('#pdfviewer', { supportApi: 'https://localhost:7133/api/pdf-viewer' });
     viewer.addDefaultPanels();
-    viewer.open(repQuickStartSample);
     viewer.addAnnotationEditorPanel();
     viewer.addFormEditorPanel();
+    viewer.open(repQuickStartSample);
