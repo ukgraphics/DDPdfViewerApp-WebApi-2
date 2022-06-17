654f35465d0732d348a3dc0c33926ff7dbc9bea9
Kuni <kuni.ujiie@grapecity.com>
2022-06-17 11:45:38 +0900

    Updated.

--- quickstart/src/index.js
+++ quickstart/src/index.js
@@ -1,10 +1,8 @@
 import * as gcpdfviewer from '@grapecity/gcpdfviewer';
-import repQuickStartSample from './pdfs/diodocs_a4_full.pdf';
+import repQuickStartSample from './pdfs/diodocs_pricelist.pdf';
 
-var viewer = new gcpdfviewer.GcPdfViewer('#pdfviewer', { supportApi: 'https://localhost:7133/api/pdf-viewer' }
-    );
+var viewer = new gcpdfviewer.GcPdfViewer('#pdfviewer', { supportApi: 'https://localhost:7133/api/pdf-viewer' });
     viewer.addDefaultPanels();
-    // viewer.open(repQuickStartSample);
+    viewer.open(repQuickStartSample);
     viewer.addAnnotationEditorPanel();
     viewer.addFormEditorPanel();
-    viewer.newDocument();
